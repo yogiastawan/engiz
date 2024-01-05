8ce273e074369a4da174394a80481b593bcfe153
Yogi Astawan <yogi.astawan@gmail.com>
2024-01-06 03:10:58 +0800

    add style

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{rs,html}"
+  ],
+  theme: {
+    screens: {
+      xs:'320px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins, sans-serif'],
+      },
+      
+    },
+  },
+  plugins: [],
+};
