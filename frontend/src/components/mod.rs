e861b34ad0d6cf700e64728cbbc74d4ce90fa972
Yogi Astawan <yogi.astawan@gmail.com>
2024-01-11 02:02:23 +0800

    init `ContributorCard`

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,3 +1,21 @@
-pub mod nav_bar;
-pub mod menu_link;
+use yew::{function_component, html, AttrValue, Html, Properties};
+
+pub mod contributor_card;
 pub mod icon;
+pub mod menu_link;
+pub mod nav_bar;
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ContProps {
+    pub name: AttrValue,
+    pub link: Option<AttrValue>,
+    pub link_type: AttrValue,
+    pub img: AttrValue,
+}
+
+#[function_component(ContributorCard)]
+pub fn contrib_card(p: &ContProps) -> Html {
+    html! {
+        <div></div>
+    }
+}
